1531ee0cb7086c1419ad60ce40698815f17a1b32
iccanobif <iccanobif@fake.mail>
2019-12-22 14:00:38 +0100

    Fix cookie problem

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 app.disableHardwareAcceleration()
 app.commandLine.appendSwitch('disable-smooth-scrolling')
 // Without disable-site-isolation-trials chrome will prevent the access to contentDocument for an iframe of different origin
-app.commandLine.appendSwitch('disable-site-isolation-trials') 
+app.commandLine.appendSwitch('disable-site-isolation-trials')
 
 let mainWindow
 
@@ -20,16 +20,38 @@ function createWindow()
   })
 
   // This is to bypass danbooru's restriction on not being loaded in iframes
-  mainWindow.webContents.session.webRequest.onHeadersReceived((detail, callback) =>
+  mainWindow.webContents.session.webRequest.onHeadersReceived({ urls: ["https://*.danbooru.donmai.us/*"] }, (detail, callback) =>
   {
-    const xFrameOriginKey = Object.keys(detail.responseHeaders).find(header => String(header).match(/^x-frame-options$/i));
+    const xFrameOriginKey = Object.keys(detail.responseHeaders).find(header => header.match(/^x-frame-options$/i));
     if (xFrameOriginKey)
-    {
       delete detail.responseHeaders[xFrameOriginKey];
+    if (detail.url == "https://danbooru.donmai.us/session")
+    {
+      const setCookieHeader = Object.keys(detail.responseHeaders).find(header => header.match(/^set-cookie$/i));
+      if (setCookieHeader)
+      {
+        const cookiesToSave = detail.responseHeaders[setCookieHeader]
+        console.log("url: " + detail.url + " cookies: " + cookiesToSave)
+        fs.writeFileSync("cookies.txt", cookiesToSave, { encoding: "utf8" })
+      }
+
     }
+
     callback({ cancel: false, responseHeaders: detail.responseHeaders });
   })
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders({ urls: ["https://*.danbooru.donmai.us/*"] }, (details, callback) =>
+  {
+    try
+    {
+      details.requestHeaders["Cookie"] = fs.readFileSync("cookies.txt", { encoding: "utf8" })
+    }
+    catch (exc)
+    {
+      console.error(exc)
+    }
+    callback({ cancel: false, requestHeaders: details.requestHeaders })
+  })
+  // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function ()
   {
     mainWindow = null
