c87a061bf74bed73a4691c5a518795435d43dc11
iccanobif <iccanobif@protonmail.com>
2020-08-04 12:31:36 +0200

    skip deleted posts with no id

--- getUrlList.js
+++ getUrlList.js
@@ -1,85 +1,89 @@
-const Danbooru = require("danbooru")
-const fs = require("fs")
-const async = require("async")
+const Danbooru = require("danbooru");
+const fs = require("fs");
+const async = require("async");
 
-let danbooruCredentials = undefined
-try
-{
-    const configText = fs.readFileSync("config.json", { encoding: "utf8" })
-    const { user, apiKey } = JSON.parse(configText)
-    danbooruCredentials = user + ":" + apiKey
-}
-catch (exc)
-{
-    // Should report the error
-    danbooruCredentials = undefined
+let danbooruCredentials = undefined;
+try {
+  const configText = fs.readFileSync("config.json", { encoding: "utf8" });
+  const { user, apiKey } = JSON.parse(configText);
+  danbooruCredentials = user + ":" + apiKey;
+} catch (exc) {
+  // Should report the error
+  danbooruCredentials = undefined;
 }
 
-const booru = new Danbooru(danbooruCredentials)
+const booru = new Danbooru(danbooruCredentials);
 
-module.exports.getUrlList = (tags, md5Blacklist) =>
-{
-    return new Promise((resolve, reject) => 
-    {
-        // Grab 10 pages' worth of posts 
-        async.map(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            (page, callback) =>
-            {
-                booru
-                    .posts({ tags: tags, limit: 200, page: page })
-                    .then(posts =>
-                    {
-                        callback(null, posts.filter(post => 
-                        {
-                            const output = !md5Blacklist.has(post.md5)
-                            return output
-                        }
-                        ))
-                    })
-                    .catch(err =>
-                    {
-                        callback(err)
-                    })
-            },
-            (err, results) =>
-            {
-                try
-                {
-                    if (err) reject(err)
-                    else
-                    {
-                        const completeList = results
-                            .reduce((acc, curr) => // Flatten arrays
-                            {
-                                return acc.concat(curr)
-                            }, [])
-                            .map(x =>"https://danbooru.donmai.us/posts/" + x.id)
+module.exports.getUrlList = (tags, md5Blacklist) => {
+  return new Promise((resolve, reject) => {
+    // Grab 10 pages' worth of posts
+    async.map(
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      (page, callback) => {
+        booru
+          .posts({ tags: tags, limit: 200, page: page })
+          .then((posts) => {
+            callback(
+              null,
+              posts.filter((post) => {
+                const output = !md5Blacklist.has(post.md5);
+                
+                return output;
+              })
+            );
+          })
+          .catch((err) => {
+            callback(err);
+          });
+      },
+      (err, results) => {
+        try {
+          if (err) reject(err);
+          else {
+            const completeList = results
+              .reduce((
+                acc,
+                curr // Flatten arrays
+              ) => {
+                return acc.concat(curr);
+              }, [])
+              .filter(x => x.id) // skip deleted posts that have no id
+              .map((x) => "https://danbooru.donmai.us/posts/" + x.id);
 
-                        resolve(completeList)
-                    }
-                }
-                catch (exc)
-                {
-                    reject(exc)
-                }
-            }
-        )
-    })
-}
+            resolve(completeList);
+          }
+        } catch (exc) {
+          reject(exc);
+        }
+      }
+    );
+  });
+};
 
-// module.exports.imagePagesIterator = async function* () {
-//     // yield 1
-//     // yield 2
-//     const tags = ["nail_polish", "legs"]
-//     let page = 1
+module.exports.imagePagesIterator = async function* (md5Blacklist) {
+  // TODO: gestire "tag" che in realta' sono una coppia di tag
+  const tags = ["nail_polish", "legs"];
+  let page = 1;
 
-//     while (true)
-//     {
-//         for (const tag of tags)
-//         {
-//             booru.posts()
-//         }
-//         page++
-//     }
-// };
+  while (true) {
+    console.log("inizio loop");
+    for (const tag of tags) {
+      console.log(tag, page);
+      const posts = await booru.posts({ tags: tag, limit: 20, page: page });
+      for (const post of posts.filter((p) => !md5Blacklist.has(p.md5))) {
+        const url = "https://danbooru.donmai.us/posts/" + post.id;
+        console.log(url);
+        yield url;
+      }
+    }
+    page++;
+  }
+};
+
+// async function kek() {
+//   for await (const val of module.exports.imagePagesIterator()) {
+//     console.log(val);
+//   }
+// }
+
+// kek().catch(console.error);
